7878ae90ea44fb5ddacb86a5e04a0fa06e0722c5
Josh Wedekind 🥚 <josh@halfnibble.com>
2020-10-24 14:44:55 -0700

    Initial alert popup.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const headerContainer = document.getElementById("masthead");
+const alertPopupEl = document.createElement("div");
+alertPopupEl.id = "alert-popup-modal";
+headerContainer?.appendChild(alertPopupEl);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  alertPopupEl
 );
 
 // If you want to start measuring performance in your app, pass a function
